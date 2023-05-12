3fd63dc480f1723fe4f5d5d8f63f0273b8acfb36
user <frank.sossi@gmail.com>
2023-05-11 17:34:55 -0700

    descriptor initial commit

--- src/sGLOH2.cpp
+++ src/sGLOH2.cpp
@@ -3,21 +3,43 @@
 //
 
 #include "sGLOH2.hpp"
-#include <opencv2/imgproc.hpp>
+#include <opencv2/xfeatures2d.hpp>
 
-sGLOH2::sGLOH2(int m) : m(m) {}
+sGLOH2::sGLOH2(int m) : m(m) {
+    // Initialize any other member variables as needed
+}
 
 void sGLOH2::compute(const cv::Mat& image, std::vector<cv::KeyPoint>& keypoints, cv::Mat& descriptors) {
-    // Implement method
+    // Create a SIFT detector
+    cv::Ptr<cv::SIFT> detector = cv::SIFT::create();
+
+    // Detect keypoints
+    detector->detect(image, keypoints);
+
+    // Compute descriptors for each keypoint
+    for (auto& keypoint : keypoints) {
+        cv::Mat patch;
+        // Extract patch around keypoint
+        // You might need to use cv::getRectSubPix or similar function
+        // Make sure to handle edge cases where the keypoint is near the border of the image
+
+        // Compute sGLOH descriptor for the patch
+        cv::Mat descriptor = compute_sGLOH(patch);
+
+        // Append descriptor to descriptors matrix
+        descriptors.push_back(descriptor);
+    }
 }
 
 cv::Mat sGLOH2::compute_sGLOH(const cv::Mat& patch) {
-    // Implement method
-    return cv::Mat();
+    // Implement your sGLOH computation here
+    // This will likely involve computing the gradient orientation histogram for the patch,
+    // and then applying the spatial and orientation weighting functions
 }
 
 double sGLOH2::distance(const cv::Mat& H_star1, const cv::Mat& H_star2) {
-    // Implement method
-    return 0.0;
+    // Implement your distance computation here
+    // This will likely involve computing the chi-squared distance between the two histograms
 }
+
 
