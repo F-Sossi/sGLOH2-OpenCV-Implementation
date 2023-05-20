1ca1a9bc2e5bd242ee6e2b0eadd5d7e3f3f5b1f0
user <frank.sossi@gmail.com>
2023-05-19 20:18:30 -0700

    1. Switched the keypoint detector to ORB 2. Re-did the SGLOH to exactly replicate the papers steps 3. Added Scaled test and 45 degree rotation test Performance is still slow but the results are better.

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,28 @@
 
 int main(int argc, char** argv) {
 
-    // tests processImage uses a flipped image processImage2 identical images
+    // processImage uses a flipped image
+    // processImage2 identical images
     // processImage3 uses a shifted image
-    //processImage2("lenna.jpg");
+    // processImage4 uses a rotated image with FLANN matching
+    // processImage5 uses a scaled with BF matching
+    // processImage6 uses a 45 degree rotation with BF matching
+    processImage4("ciri.jpg");
 
 
-    //create imagematcher
-    ImageMatcher imageMatcher;
-    cv::Mat image = cv::imread("../rin.jpg", cv::IMREAD_GRAYSCALE);
-    cv::imshow("Query", image);
-
-    //Search images using sift
-    std::cout << "Perform SIFT matching" << std::endl;
-    std::vector<cv::Mat> siftImages = imageMatcher.siftMatch(image);
-    std::cout << "Number of images above threshold: " << siftImages.size() << std::endl;
-    for (int i = 0; i < siftImages.size(); i++) {
-        cv::imshow("Image", siftImages[i]);
-        cv::waitKey(0);
-    }
+//    //create imagematcher
+//    ImageMatcher imageMatcher;
+//    cv::Mat image = cv::imread("../rin.jpg", cv::IMREAD_GRAYSCALE);
+//    cv::imshow("Query", image);
+//
+//    //Search images using sift
+//    std::cout << "Perform SIFT matching" << std::endl;
+//    std::vector<cv::Mat> siftImages = imageMatcher.siftMatch(image);
+//    std::cout << "Number of images above threshold: " << siftImages.size() << std::endl;
+//    for (int i = 0; i < siftImages.size(); i++) {
+//        cv::imshow("Image", siftImages[i]);
+//        cv::waitKey(0);
+//    }
     //Search images using sGlOH2
 //    std::cout << "Perform sGLOH2 matching" << std::endl;
 //    std::vector<cv::Mat> sgloh2Images = imageMatcher.sGLOHMatch(image, M);
