c70ee4aa64dae9ccbc10520a2b22181f7f6c358a
Justin Boyer <justinbboyer@gmail.com>
2023-05-30 21:47:38 -0700

    Run test 10 times

--- test_scripts/synthetic_rotation.py
+++ test_scripts/synthetic_rotation.py
@@ -8,8 +8,12 @@ imageInputPath = "C:\\Users\\Justin\\CLionProjects\\sGLOH_opencv\\src_img\\touca
 folderPath = "C:\\Users\\Justin\\CLionProjects\\sGLOH_opencv\\images"
 args = [imageInputPath, folderPath]
 
-# Run the executable and capture its output
-result = subprocess.run([executable_path] + args, stdout=subprocess.PIPE)
+# Open the output file in append mode
+with open('output.txt', 'a') as f:
+    # Run the program 10 times
+    for i in range(10):
+        # Run the executable and redirect its output to the file
+        subprocess.run([executable_path] + args, stdout=f)
 
-# Print the output
-print(result.stdout.decode('utf-8'))
+        # Write a newline character to separate the output from each run
+        f.write('\n')
