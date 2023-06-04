281cfd1081f0aac88682965858fa307fb26fee74
Justin Boyer <justinbboyer@gmail.com>
2023-06-03 19:08:51 -0700

    Update scripts

--- test_scripts/tests.py
+++ test_scripts/tests.py
@@ -11,9 +11,13 @@ args = [imageInputPath, folderPath]
 # Get a list of all the query images in the src_img folder to iterate over in the test loop
 query_images = [f for f in os.listdir('src_img') if os.path.isfile(os.path.join('src_img', f))]
 # Open the output file in write mode
-with open('output.txt', 'w') as f:
+with open('output2.txt', 'w') as f:
     # Iterate over the query images
     for query_image in query_images:
+        imageInputPath = "C:\\Users\\Justin\\CLionProjects\\sGLOH_opencv\\src_img\\" + query_image
+        folderPath = "C:\\Users\\Justin\\CLionProjects\\sGLOH_opencv\\images"
+        args = [imageInputPath, folderPath]
+        f.write('Testing: ' + query_image + '\n')
         # Run the executable and redirect its output to the file
         subprocess.run([executable_path] + args, stdout=f)
 
@@ -21,4 +25,4 @@ with open('output.txt', 'w') as f:
         f.write('\n')
 
 # Run the clean script on the output file
-subprocess.run(['python', 'clean.py', 'output.txt'])
+subprocess.run(['python', "C:\\Users\\Justin\\CLionProjects\\sGLOH_opencv\\test_scripts\\clean.py", 'output.txt'])
